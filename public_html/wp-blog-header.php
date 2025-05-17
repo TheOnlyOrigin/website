bbaa210bc180189b1209bec4c7226957e933d30f
The Origin <paul@IamtheOrigin.org>
2025-05-16 22:46:40 -0700

    Add missing WordPress bootstrap files

---
+++ public_html/wp-blog-header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * This is a minimal placeholder to allow index.php to load
+ * WordPress. Full WordPress core files are not included in this
+ * repository.
+ *
+ * @package WordPress
+ */
+
+require_once __DIR__ . '/wp-load.php';
+/*
+ * Normally wp() and template-loader would be called here, but
+ * these functions depend on the full WordPress core. They are
+ * omitted in this placeholder.
+ */
