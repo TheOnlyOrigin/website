7dfad24eefa0c0bc7f1f4aa50787a709640f295f
The Origin <paul@IamtheOrigin.org>
2025-05-16 22:48:34 -0700

    Extract WordPress archive and add blog header

---
+++ public_html/wp-blog-header.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * Placeholder for WordPress entry point.
+ */
+
+require __DIR__ . '/wp-load.php';
+wp();
+require_once ABSPATH . WPINC . '/template-loader.php';
+
